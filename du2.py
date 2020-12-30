41b32069d70eeb658c8262a9290e2f9adedc9226
Petr Janoš <koksa.janos@gmail.com>
2020-12-31 00:40:34 +0100

    volně přístupné kontejnery, basic

--- du2.py
+++ du2.py
@@ -7,7 +7,7 @@ def nahraj_geojson(jmeno_souboru):
                 data = json.load(file)
 
     except FileNotFoundError:
-        print(f"Soubor {jmeno_souboru} nenalezen.")
+        print(f"Soubor {jmeno_souboru} nebyl nenalezen.")
         exit()
     except ValueError:
         print(f"Soubor {jmeno_souboru} je chybný.")
@@ -17,3 +17,27 @@ def nahraj_geojson(jmeno_souboru):
         exit()
 
     return data 
+
+
+def data_kontejnery(kontejnery):
+    """ Vytvoří slovník pro volně přístupné kontejnery [adresa:souřadnice]. """
+
+    dic_kontejnery = {} 
+
+    for k in kontejnery:
+        k_adresa = k["properties"]["STATIONNAME"]
+        k_geo = k["geometry"]["coordinates"]
+        k_pristup = k["properties"]["PRISTUP"]
+
+        if k_pristup == "volně":
+            dic_kontejnery[k_adresa] = k_geo
+
+    if len(dic_kontejnery) == 0:
+        print("Nebyly nalezeny žádné volně přístupné kontejnery. ")
+
+    return dic_kontejnery
+
+
+
+kontejnery = nahraj_geojson("kontejnery")["features"]
+print(data_kontejnery(kontejnery))
