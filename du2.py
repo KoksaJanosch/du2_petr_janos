870d3d547ab99938f5e2cd9e378f4142cdb0ed68
Petr Janoš <koksa.janos@gmail.com>
2020-12-31 19:14:06 +0100

    otestování funkčnosti na malém počtu dat (kontejnery,adresy)

--- du2.py
+++ du2.py
@@ -1,4 +1,5 @@
 import json, os, sys
+from pyproj import CRS, Transformer
 
 def nahraj_geojson(jmeno_souboru):
     """ Načte soubor typu geojson a ošetří nekorektní vstupy. """
@@ -32,30 +33,36 @@ def data_kontejnery(kontejnery):
         if k_pristup == "volně":
             dic_kontejnery[k_adresa] = k_geo
 
-    if len(dic_kontejnery) == 0:
-        print("Nebyly nalezeny žádné volně přístupné kontejnery. ")
-
+    
     return dic_kontejnery
 
 def data_adresy(adresy):
-    """ pass """
+    """ Vytvoří slovník pro adresní body [adresa:souřadnice] a převede je na S-JTSK"""
 
     dic_adresy = {}
 
     for a in adresy:
         a_ulice = a["properties"]["addr:street"]
         a_cp = a["properties"]["addr:housenumber"]
-        a_geo = a["geometry"]["coordinates"]
+        a_geo_wgs = a["geometry"]["coordinates"]
 
+        #? WGS » S-JTSK
+        # nutnost použít crs a transformer z pyproj2 (náhrada pyproj1)
+        wgs = CRS.from_epsg(4326)  # WGS-84
+        jtsk = CRS.from_epsg(5514)  # S-JTSK
+        wgis_jtsk = Transformer.from_crs(wgs, jtsk)
+
+        a_geo_jtsk = wgis_jtsk.transform(a_geo_wgs[1], a_geo_wgs[0])
         a_adresa = a_ulice + " " + a_cp
-
-        dic_adresy[a_adresa] = a_geo
+ 
+        dic_adresy[a_adresa] = a_geo_jtsk
 
     return dic_adresy
 
-# ? zavedení proměnných
+# ? načtení vstupních dat 
 kontejnery_json = nahraj_geojson("kontejnery")["features"]
 adresy_json = nahraj_geojson("adresy")["features"]
 
 print(data_adresy(adresy_json))
-
+print("____________ \n ")
+print(data_kontejnery(kontejnery_json))
